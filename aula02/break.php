ff3bece274a1f819914ebedf5b02aaa1c489ead5
Lucas Marques <lucasmarques73@hotmail.com>
2018-05-26 12:07:21 -0300

    break continue

---
+++ aula02/break.php
@@ -0,0 +1,11 @@
+<?php 
+
+// Numeros de 0 a 10, para quando chegar no 6
+
+for ($i=0; $i <= 10 ; $i++) { 
+	if ($i == 6) {
+		break;
+	}
+
+	echo $i.PHP_EOL;
+}
---
+++ aula02/continue.php
@@ -0,0 +1,11 @@
+<?php 
+
+// Numeros de 0 a 10 pulando o 7
+
+for ($i=0; $i <= 10 ; $i++) { 
+	if ($i == 7) {
+		continue;
+	}
+
+	echo $i.PHP_EOL;
+}
