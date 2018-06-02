8be14db38b2d27c548720790aaf869bdb149435a
Lucas Marques <lucasmarques73@hotmail.com>
2018-06-02 15:29:00 -0300

    web

---
+++ aula03/header.php
@@ -0,0 +1,7 @@
+<?php 
+
+// Location
+// header('location: https://google.com');
+
+header('Content-type: text/xml');
+// header('Content-type: application/pdf');
---
+++ aula03/superglobais.php
@@ -0,0 +1,50 @@
+<?php 
+
+echo '<pre>';
+
+// print_r($_SERVER);
+
+// $_ENV['bd'] = 'dev';
+
+// print_r($_ENV);
+
+// .php?nome=Lucas&email=lucas@lucas.com
+
+// print_r($_GET);
+
+// if (isset($_GET['nome'])) {
+// 	echo $_GET['nome'];
+// }
+
+
+// echo '<form method="POST">
+// 	<input type="text" name="nome">
+// 	<button type="submit">OK</button>
+// </form>';
+
+// print_r($_POST);
+
+// session_start();
+// $_SESSION['nome'] = 'Lucas';
+
+// print_r($_SESSION);
+
+// setcookie('cor','preto',strtotime('1 day'));
+// print_r($_COOKIE);
+
+
+// print_r($_REQUEST);
+
+// echo '<form method="POST" enctype="multipart/form-data">
+// 	<input type="text" name="nome">
+// 	<input type="file" name="img">
+// 	<button type="submit">OK</button>
+// </form>';
+
+// print_r($_FILES);
+// echo '<hr>';
+// print_r($_POST);
+
+$nome = '4linux';
+
+print_r($GLOBALS);
